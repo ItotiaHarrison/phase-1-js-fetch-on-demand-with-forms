37678cc910cbd89cd38cc8f0e32444d40b78f480
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-08-25 14:49:18 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const userInput = document.getElementById("searchByID");
+  
+      fetch(`http://localhost:3000/movies/${userInput.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        });
+    });
   
 }
 
